3e43677caa1e30614921f47b63776d345c09da7c
shigemk2 <i.am.shige@gmail.com>
2015-01-20 22:43:09 +0900

    StateTモナドはexecStateもrunStateもいける

--- statet_identify.hs
+++ statet_identify.hs
@@ -4,3 +4,5 @@ import Control.Monad.State
 main = do
     let a = return 1 :: StateT s Identity Int  -- StateTとして生成
     print $ evalState a ()                     -- Stateとして評価
+    print $ execState a ()                     -- Stateとして評価
+    print $ runState a ()                      -- Stateとして評価
