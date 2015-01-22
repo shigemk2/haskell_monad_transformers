4544d7109e3e15e4125767222e296232cc9c2be0
shigemk2 <i.am.shige@gmail.com>
2015-01-22 15:03:47 +0900

    再実装

---
+++ re-implement1.hs
@@ -0,0 +1,9 @@
+import Control.Monad.Identity
+import Control.Monad.State
+
+return' x = StateT $ \s -> Identity (1, s)
+runState' st = return $ runIdentity $ runStateT st ()
+
+main = do
+    let st = return' 1
+    print $ runState' st ()
