dcbcfa28fdc8e34b242e37a127d694b48150ac1c
shigemk2 <i.am.shige@gmail.com>
2015-01-26 14:38:38 +0900

    fact書き直し その2

--- re-implement2.hs
+++ re-implement2.hs
@@ -2,9 +2,10 @@ import Control.Monad
 import Control.Monad.State
 
 fact x = (`execStateT` 1) $ do
-    forM_ [1..x] $ \i ->
-        modify (* i) >>= \_ ->
-        get >>= \v ->
+    forM_ [1..x] $ \i -> do
+        modify (* i)
+        v <- get
         lift $ putStrLn $ "*" ++ show i ++ " -> " ++ show v
 
-main = fact 5 >>= print
+main = do
+  print =<< fact 5
