57b42383da12c363840e2f01f6d84a5351c99738
shigemk2 <i.am.shige@gmail.com>
2015-01-28 14:44:05 +0900

    再実装

--- re-implement3.hs
+++ re-implement3.hs
@@ -1,6 +1,14 @@
 import Control.Monad
 import Control.Monad.State
 
+a `bind` b = StateT $ \s ->
+    runStateT  a    s >>= \(r, s1) ->
+    runStateT (b r) s1
+
+get' = StateT $ \s -> return (s, s)
+modify' f = StateT $ \s -> return ((), f s)
+lift' m = StateT $ \s -> m >>= \a -> return (a, s)
+
 fact x = (`execStateT` 1) $
     forM_ [1..x] $ \i ->
         modify' (* i) `bind` \_ ->
