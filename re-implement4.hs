f07ef0fc41eef08fbc64f68bf7d53d660c6eb1c0
shigemk2 <i.am.shige@gmail.com>
2015-01-31 20:56:27 +0900

    リスト内包表記 再実装

--- re-implement4.hs
+++ re-implement4.hs
@@ -1,3 +1,7 @@
+import Control.Monad
+
 main = do
     print [(x , y) | x <- [0, 1], y <- [0, 2]]
     print [ x + y  | x <- [0, 1], y <- [0, 2]]
+    print $ liftM2 (,) [0,1] [0,2]
+    print $ liftM2 (+) [0,1] [0,2]
